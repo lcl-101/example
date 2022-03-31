8b992fa4ed7899b8716c9112e93f625f86fae895
lichenglong04 <lichenglong04@daojia-inc.com>
2022-03-31 09:45:42 +0800

    ++

---
+++ vuebaipingtest/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld :msg="test"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      test: {}
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  created () {
+    console.log('a-created')
+    this.test = {
+      name: 123,
+      img: ''
+    }
+    console.log(this.test.img.a.indexOf('a'))
+    console.log('test')
+  },
+  mounted (){
+    console.log('a-mounted')
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vuebaipingtest/src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    {{msg.name}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: Object
+  },
+  created () {
+    console.log('b-created')
+    console.log(this.msg.img)
+    this.msg.img.indexOf('b')
+  },
+  mounted (){
+    console.log('b-mounted')
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
